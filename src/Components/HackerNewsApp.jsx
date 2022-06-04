8860785f2878349321a95793206298368c0c16a3
Dušan Jonjin <68252940+DusanJonjin@users.noreply.github.com>
2022-06-04 14:40:44 +0200

    Small color change on header fonts

--- src/Components/HackerNewsApp.jsx
+++ src/Components/HackerNewsApp.jsx
@@ -1,4 +1,4 @@
-import React, {Suspense, lazy} from 'react';
+import React from 'react';
 import { Header } from './Header/Header';
 import { Stories } from './Stories/Stories';
 import { Comments } from './Comments/Comments';
