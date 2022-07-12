924ea2c42060f4611cf71e5ff4a75ee12b63d840
Dušan Jonjin <68252940+DusanJonjin@users.noreply.github.com>
2022-07-12 03:07:37 +0200

    Added a StoryCommentOptions component

---
+++ src/Components/- Shared -/StoryCommentOptions.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { collapseExpandComment, getScrolledCommentID, getRepliesCount } from '../../Store/actions';
+
+export function StoryCommentOptions(props) {
+
+    const {
+        commentID,
+        dark,
+        modern,
+        dispatch,
+        themedClass,
+        repliesCount,
+        commentIsExpanded,
+        goToComment
+    } = props;
+
+    const {
+        nestLevel,
+        root,
+        parent,
+        prev,
+        next
+    } = goToComment;
+
+    return (
+        <div className={themedClass('comment-exp-collapse', dark, modern)}>
+            {nestLevel > 1 ? <p onClick={() => dispatch(getScrolledCommentID(root))}>root</p> : ''}
+            {parent !== 0 ? <p onClick={() => dispatch(getScrolledCommentID(parent))}>parent</p> : ''}
+            {prev !== 0 ? <p onClick={() => dispatch(getScrolledCommentID(prev))}>prev</p> : ''}
+            <p onClick={() => dispatch(getScrolledCommentID(next))}>next</p>
+            <p 
+                onClick={() => (
+                    !repliesCount && dispatch(getRepliesCount(commentID)),
+                    dispatch(collapseExpandComment(commentID))
+                )}
+            >
+                {commentIsExpanded ? `[ - ]` : `[ ${repliesCount} more ]`}
+            </p>
+            {nestLevel > 0 
+            ?   <p 
+                    onClick={() => (
+                        !repliesCount && dispatch(getRepliesCount(root)),
+                        dispatch(collapseExpandComment(root)),
+                        dispatch(getScrolledCommentID(root))
+                    )}
+                >
+                    [ x ]
+                </p> 
+            :   ''
+            }
+            
+        </div>
+    )
+}
